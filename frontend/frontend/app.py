173f62c031d3db21dd9532a7cf360e1625017cb6
Ravroop Kaur <ravroop0310@gmail.com>
2026-07-28 23:11:25 +0530

    Update app.py

--- frontend/frontend/app.py
+++ frontend/frontend/app.py
@@ -13,7 +13,7 @@ from views.scan import render_scan
 
 st.set_page_config(
     page_title="MediSafe AI",
-    page_icon= FAVICON_IMAGE, 
+    page_icon= str(FAVICON_IMAGE), 
     layout="wide",
     initial_sidebar_state="collapsed",
 )
